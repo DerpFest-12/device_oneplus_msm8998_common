0033bce15d2ce61c29fd58028af67eb3149e2782
NurKeinNeid <mralexman3000@gmail.com>
2021-12-14 23:12:46 +0100

    Revert "msm8998-common: update IMS from Nokia daredevil"
    This reverts commit a70cef60783056910f8265e3cca2eb9a086413c5.

--- common.mk
+++ common.mk
@@ -297,10 +297,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
-PRODUCT_PACKAGES += \
-    libavservices_minijail \
-    libavservices_minijail.vendor
-
 # Media Extensions
 PRODUCT_PACKAGES += \
     libmediametrics \
@@ -392,10 +388,9 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.5.vendor \
-    android.hardware.radio.config@1.2.vendor \
-    android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.secure_element@1.2.vendor \
+    android.hardware.radio@1.1.vendor \
+    android.hardware.radio.config@1.0.vendor \
+    android.hardware.secure_element@1.0.vendor \
     librmnetctl \
     libxml2 \
     libprotobuf-cpp-full
